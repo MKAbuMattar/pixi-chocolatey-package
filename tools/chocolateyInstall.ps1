6a2bd1e4f228a89100dda5c5440d7c1fcc156053
MKAbuMattar <mohammad.khaled@outlook.com>
2025-12-10 20:25:44 +0300

    Refactor chocolateyInstall.ps1 to use Get-ChocolateyWebFile for downloading the Pixi executable

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,14 +1,13 @@
 $ErrorActionPreference = 'Stop'
 
+$toolsPath = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
-  fileType       = 'EXE'
   url64          = 'https://github.com/prefix-dev/pixi/releases/download/v0.61.0/pixi-x86_64-pc-windows-msvc.exe'
   checksum64     = 'B9F4FA937A3FCF688671CD0DAB4D06469684E7AFB03A5CC8A42388CC7D61B971'
   checksumType64 = 'sha256'
-  softwareName   = 'pixi*'
-  silentArgs     = '/S /quiet'
-  validExitCodes = @(0, 3010, 1641)
+  fileFullPath   = Join-Path $toolsPath 'pixi.exe'
 }
 
-Install-ChocolateyPackage @packageArgs
+Get-ChocolateyWebFile @packageArgs
