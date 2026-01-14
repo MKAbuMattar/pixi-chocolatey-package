ad4529b9afb1f60307eee4e3e9d43c428f30af49
MKAbuMattar <mohammad.khaled@outlook.com>
2026-01-14 15:18:15 +0000

    Update pixi to v0.63.0

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -4,8 +4,8 @@ $toolsPath = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
-  url64          = 'https://github.com/prefix-dev/pixi/releases/download/v0.62.2/pixi-x86_64-pc-windows-msvc.exe'
-  checksum64     = '67041E866C82E354C13E4524089035E675122B47328BA209E6A4A7AC3E147793'
+  url64          = 'https://github.com/prefix-dev/pixi/releases/download/v0.63.0/pixi-x86_64-pc-windows-msvc.exe'
+  checksum64     = 'A8F1A2D38005DAD92C646FC1AFCF3205F51E9C277A72365680420322F924C884'
   checksumType64 = 'sha256'
   fileFullPath   = Join-Path $toolsPath 'pixi.exe'
 }
